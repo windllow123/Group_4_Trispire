8fc9b63adf2a89f72fed8cf1e14c58061fdc25f3
Noctua-Yz <youzhi.guo@outlook.com>
2026-05-03 19:19:04 +0800

    Updated documentation
    Added function documentation comments for clarity.

--- Card.cpp
+++ Card.cpp
@@ -1,9 +1,13 @@
 #include "Card.h"
 
+// Function: Constructs card and initializes its name based on type
+// Input: t - the card type
 Card::Card(CardType t) : type(t) {
     reset(t);
 }
 
+// Function: Changes card type and updates name accordingly
+// Input: t - the new card type to set
 void Card::reset(CardType t) {
     type = t;
     switch (type) {
@@ -14,10 +18,15 @@ void Card::reset(CardType t) {
     }
 }
 
+// Function: Gets the display name of the card
+// Output: the card's name string
 std::string Card::getName() const {
     return name;
 }
 
+// Function: Acquires a card from pool or creates new if none available
+// Input: t - type of card to acquire
+// Output: pointer to the card
 Card* CardPool::acquire(CardType t) {
     if (!free_cards.empty()) {
         Card* card = free_cards.back();
@@ -29,6 +38,8 @@ Card* CardPool::acquire(CardType t) {
     return owned_cards.back().get();
 }
 
+// Function: Returns a card to the free pool for reuse
+// Input: card - pointer to the card to release
 void CardPool::release(Card* card) {
     if (card) {
         free_cards.push_back(card);
